de6853445bc907557b7126d14faa19fc34fbb206
Rhuan Martins <rhuan.gabriel2001@icloud.com>
2022-12-11 22:52:38 -0300

    Commit de dir refactor

--- old-code/index.js
+++
@@ -1,9 +0,0 @@
-const app = require("./config/server");
-const routes = require("./app/routes/routes");
-
-// routes.home(server);
-// routes.account(server);
-// routes.signUp(server);
-// routes.addUser(server);
-// routes.authenticateUser(server);
-// routes.insertBook(server);
